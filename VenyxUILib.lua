1b5d047c2b7feae1eee0e53bcb4f17bb6559ff2b
1NF1n <59954102+1NF1n@users.noreply.github.com>
2021-04-23 22:20:42 -0700

    Update VenyxUILib.lua

--- VenyxUILib.lua
+++ VenyxUILib.lua
@@ -15,11 +15,11 @@ local utility = {}
 local objects = {}
 local themes = {
     	Background = Color3.fromRGB(24, 24, 24),
-    	Glow = Color3.fromRGB(0, 255, 255),
+    	Glow = Color3.fromRGB(255, 0, 217),
     	Accent = Color3.fromRGB(10, 10, 10),
     	LightContrast = Color3.fromRGB(36, 36, 36),
     	DarkContrast = Color3.fromRGB(27, 27, 27),  
-    	TextColor = Color3.fromRGB(0, 255, 255)
+    	TextColor = Color3.fromRGB(255, 0, 217)
 }
 
 do
